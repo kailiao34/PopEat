a019ef2353f127958203e29fbf45a268b86b6669
Eddy <kailiao34@gmail.com>
2018-03-29 13:19:58 +0800

    更新取得餐廳資訊API (未完成)

--- Assets/Eddy/Scripts/Tests/WWWTest.cs
+++ Assets/Eddy/Scripts/Tests/WWWTest.cs
@@ -3,42 +3,55 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class WWWTest : MonoBehaviour {
-    public FoodLis foodLis;
+    //public FoodLis foodLis;
     void Start() {
-		GetRes.ins.Get(24.99579212, 121.48876185, 500, PrintNames);
+		System.Text.StringBuilder b = new System.Text.StringBuilder();
+		b.Append(" ");
+		b.Append(3).Append(" ");
+		b.Append(5.45f).Append(" sdf");
 
+		print(b.ToString());
+
+		//System.DateTime d = new System.DateTime(1995,6,7,8,9,1);
+		//print(d);
+
+		//System.DateTime d1 = new System.DateTime(d.Ticks);
+		//print(d1);
+
+		//GetRes.ins.GetAllRes(24.99579212, 121.48876185, 500, PrintDetails);
+		//GetRes.ins.Get(24.99579212, 121.48876185, 500, PrintNames);
 	}
 
-	void PrintNames(string[] resNames) {
-		foreach (string s in resNames) {
-			GetRes.ins.GetResDetails(s, PrintDetails);
+	//void PrintNames(string[] resNames) {
+	//	foreach (string s in resNames) {
+	//		GetRes.ins.GetResDetails(s, PrintDetails);
+ //       }
+ //   }
 
-        }
-    }
+	void PrintDetails(List<Details> d) {
 
-	void PrintDetails(Details dd) {
+		foreach (Details dd in d) {
+			print(dd.name);
+			//foodLis.list.Add(dd.name);
 
-		print(dd.name);
-		foodLis.list.Add(dd.name);
+			print(dd.address);
+			print(dd.phoneNum);
+			foreach (string s in dd.openingHours) {
+				print(s);
+			}
+			print(dd.rating);
+			print("--------- reviews ----------");
+			foreach (Reviews r in dd.reviews) {
+				print(r.name);
+				print(r.rating);
+				print(r.text);
+				print(r.time);
+			}
 
-		print(dd.address);
-		print(dd.phoneNum);
-		foreach (string s in dd.openingHours) {
-			print(s);
+			print("----------------------------");
+			print(dd.permanentlyClosed);
+			print("=======================");
+			//foodLis.Getlist();
 		}
-		print(dd.rating);
-		print("--------- reviews ----------");
-		foreach (Reviews r in dd.reviews) {
-			print(r.name);
-			print(r.rating);
-			print(r.text);
-			print(r.time);
-		}
-
-		print("----------------------------");
-		print(dd.permanentlyClosed);
-		print(dd.requested);
-		print("=======================");
-		foodLis.Getlist();
 	}
 }
---
+++ Assets/SendTest.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SendTest : MonoBehaviour {
+
+	Details d;
+
+	void Start () {
+		d = new Details();
+		d.name = "name";
+		d.address = "address";
+		d.phoneNum = "phoneNum";
+		d.openingHours = new List<string>() { "h1", "h2", "h3" };
+		d.rating = 3.1415f;
+		d.reviews = new List<Reviews>() {
+			new Reviews() { name = "rName", rating = 1, text = "text1", time = new System.DateTime(1995,6,7,8,9,1) },
+			new Reviews() { name = "rName1", rating = 2, text = "text2", time = new System.DateTime(1995,9,10,8,9,1) }
+		};
+		d.permanentlyClosed = true;
+
+		Send();
+	}
+
+	void Send() {
+		string str = "";
+		str += d.name + " ";
+		str += d.address + " ";
+		str += d.phoneNum + " ";
+		str += d.openingHours.Count;
+
+	}
+}
