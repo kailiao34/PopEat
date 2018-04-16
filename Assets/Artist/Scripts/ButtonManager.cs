5e0ed3fc0f009150510952c96f0e5e8af5cec2b8
BlackRice <jesenke@hotmail.com>
2018-04-17 00:21:48 +0800

    主選單 UI 切換大抵搞定。

--- Assets/Artist/Scripts/ButtonManager.cs
+++ Assets/Artist/Scripts/ButtonManager.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.EventSystems;
 
 public class ButtonManager : MonoBehaviour {
 
@@ -9,6 +10,8 @@ public class ButtonManager : MonoBehaviour {
     public Animator UIswitcher;
 
     public Button[] buttons;
+    //public Sprite[] spriteImages;
+    public Text errorText; // 用來顯示 錯誤訊息的 UI Text;
 
     private void Awake()
     {
@@ -33,7 +36,7 @@ public class ButtonManager : MonoBehaviour {
         {
             UIswitcher1.SetBool("nick", false);
         }
-        else print("這不該發生!");
+        else print("這不該發生! Nick");
     }
 
     //=======呼叫餐廳列表介面==========
@@ -49,22 +52,40 @@ public class ButtonManager : MonoBehaviour {
             UIswitcher1.SetBool("eat", false);
             //buttons[1].enabled = true;
         }
-        else print("這不該發生!");
+        else print("這不該發生! Eat");
     }
 
     //=======呼叫進入/創立房間介面==========
 
+    // 雖然共用，但創房 和 進房 還是需要分別 (color)
+
     public void buttonCer()
     {
+        Button buttonCreateR = buttons[1];
+        Button buttonEnterR = buttons[4];
+
         if (UIswitcher1.GetBool("cer") == false)
         {
+            //BR - EventSystem 用來判定是創房 還是 進房。 cerF 1 = 創房按鈕 / 0 = 進房按鈕;
+            if (EventSystem.current.currentSelectedGameObject.name == buttonCreateR.name)
+            {
+                UIswitcher1.SetInteger("cerF", 1);
+                //print("111");
+            }
+            else if (EventSystem.current.currentSelectedGameObject.name == buttonEnterR.name)
+            {
+                UIswitcher1.SetInteger("cerF", 0);
+                //print("000");
+            }
+            else print("不是創房也非進房，這不該發生!");
+
             UIswitcher1.SetBool("cer", true);
         }
         else if (UIswitcher1.GetBool("cer") == true)
         {
             UIswitcher1.SetBool("cer", false);
         }
-        else print("這不該發生!");
+        else print("這不該發生! Cer");
     }
 
     //=======呼叫等候室介面==========
@@ -72,29 +93,48 @@ public class ButtonManager : MonoBehaviour {
     public void buttonWait()
     {
         Button buttonAllGo = buttons[0];
-        //Button buttonEscape = buttons[5];
+        Button buttonEscape = buttons[5];        
 
-        //need Escape Yes No UI
+        //need Escape Yes No UI - Posponed
 
         if (UIswitcher1.GetBool("wait") == false && buttonAllGo.isActiveAndEnabled)
         {
             UIswitcher1.SetBool("wait", true);
         }
-        else if (UIswitcher1.GetBool("wait") == true && /*buttonEscape.isActiveAndEnabled*/)
+        else if (UIswitcher1.GetBool("wait") == true && buttonEscape.isActiveAndEnabled)
         {
             UIswitcher1.SetBool("wait", false);
         }
-        else print("這不該發生!");
+        else print("這不該發生! Wait");
     }
 
-    /*public void buttonNick()
+    //=======呼叫錯誤提示介面==========
+    public void ErrorMsg()
     {
-        EnterFoodListButtonAnimator.SetBool("switch", true);
-        if (FoodInputField.text == "")
+        if (UIswitcher1.GetBool("error") == false)
         {
-            EnterFoodListButtonAnimator.SetBool("switch", false);
-            Debug.Log("目前沒有文字");
+            UIswitcher1.SetBool("error", true);
+            errorText.text = "想寫啥就換成啥";         //錯誤訊息由這邊置換
         }
-        EnterFoodListButton.UItext.text = FoodInputField.text;
-    }*/
+        else if (UIswitcher1.GetBool("error") == true)
+        {
+            UIswitcher1.SetBool("error", false);
+            errorText.text = "";            
+        }
+        else print("這不該發生! error");
+    }
+
+    //=======呼叫 Credits 介面==========
+    public void CreditList()
+    {
+        if (UIswitcher1.GetBool("credits") == false)
+        {
+            UIswitcher1.SetBool("credits", true);            
+        }
+        else if (UIswitcher1.GetBool("credits") == true)
+        {
+            UIswitcher1.SetBool("credits", false);
+        }
+        else print("這不該發生! credits");
+    }
 }
