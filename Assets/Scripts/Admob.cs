7122322e6cd767518da8936138a5f47af499e618
BlackRice <jesenke@hotmail.com>
2018-05-09 01:09:28 +0800

    AdMob BannerView 加到 Hall 場景

---
+++ Assets/Scripts/Admob.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using UnityEngine;
+using GoogleMobileAds.Api;
+
+public class Admob : MonoBehaviour {
+
+    public BannerView bannerView;
+
+    public void Start()
+    {
+        #if UNITY_ANDROID
+            //string appId = "ca-app-pub-2763403976639382~3279308884"; //這是 PopEat Android 的 appId
+            string appId = "ca-app-pub-3940256099942544~3347511713"; //這是測試用的 appID
+        #elif UNITY_IPHONE
+            //尚未添加  註： Google Play 和 Apple 用的是不同 ID!!!
+            string appId = "not_yet_added";
+        #else
+            string appId = "unexpected_platform";
+        #endif
+
+        // Initialize the Google Mobile Ads SDK.
+        MobileAds.Initialize(appId);
+
+        RequestBanner();
+        bannerView.Show();
+    }
+
+    public void RequestBanner()
+    {
+        #if UNITY_ANDROID
+            //string adUnitId = "ca-app-pub-2763403976639382/9852456090"; //這是 Banner1 的 adUnitId
+            string adUnitId = "ca-app-pub-3940256099942544/6300978111"; //這是測試用的 adUnitId
+        #elif UNITY_IPHONE
+            //尚未添加  註： Google Play 和 Apple 用的是不同 ID!!!    
+            string adUnitId = "not_yet_added";
+        #else
+            string adUnitId = "unexpected_platform";
+        #endif
+
+        // Create a 320x50 banner at the top of the screen.
+        // bannerView = new BannerView(adUnitId, AdSize.Banner, AdPosition.Top);
+        bannerView = new BannerView(adUnitId, AdSize.SmartBanner, AdPosition.Top);
+
+        //Banner behaviour 參數
+        // Called when an ad request has successfully loaded.
+        bannerView.OnAdLoaded += HandleOnAdLoaded;
+        // Called when an ad request failed to load.
+        bannerView.OnAdFailedToLoad += HandleOnAdFailedToLoad;
+        // Called when an ad is clicked.
+        bannerView.OnAdOpening += HandleOnAdOpened;
+        // Called when the user returned from the app after an ad click.
+        bannerView.OnAdClosed += HandleOnAdClosed;
+        // Called when the ad click caused the user to leave the application.
+        bannerView.OnAdLeavingApplication += HandleOnAdLeftApplication;
+
+        // Load a banner ad.
+        bannerView.LoadAd(createAdRequest());
+    }
+
+    public AdRequest createAdRequest()
+    {
+        return new AdRequest.Builder()
+            // .AddTestDevice(AdRequest.TestDeviceSimulator)
+            //.AddTestDevice("27f3d8fe5cf2553d0f66a3a05687ac70")
+                .TagForChildDirectedTreatment(false)
+                .Build();
+    }
+
+    public void HandleOnAdLoaded(object sender, EventArgs args)
+    {
+        MonoBehaviour.print("HandleAdLoaded event received");
+    }
+
+    public void HandleOnAdFailedToLoad(object sender, AdFailedToLoadEventArgs args)
+    {
+        MonoBehaviour.print("HandleFailedToReceiveAd event received with message: "
+                            + args.Message);
+    }
+
+    public void HandleOnAdOpened(object sender, EventArgs args)
+    {
+        MonoBehaviour.print("HandleAdOpened event received");
+    }
+
+    public void HandleOnAdClosed(object sender, EventArgs args)
+    {
+        MonoBehaviour.print("HandleAdClosed event received");
+    }
+
+    public void HandleOnAdLeftApplication(object sender, EventArgs args)
+    {
+        MonoBehaviour.print("HandleAdLeftApplication event received");
+    }
+}
